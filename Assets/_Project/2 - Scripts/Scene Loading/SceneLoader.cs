36a6dfeef2d4807eb1baf00496f8ccfed9758289
Victor Aubry <va.vic.aub@gmail.com>
2022-10-01 00:59:12 +0200

    Add scene loading

---
+++ Assets/_Project/2 - Scripts/Scene Loading/SceneLoader.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+
+    [SerializeField] private GameObject _loaderCanvas;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Load(string sceneName)
+    {
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        _loaderCanvas.SetActive(true);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = false;
+
+        // Wait until the asynchronous scene fully loads
+        while (asyncLoad.progress < 0.9f)
+        {
+            yield return null;
+        }
+        asyncLoad.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
+    }
+}
